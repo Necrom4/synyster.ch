33ed70ac59d100ae9e74c5af73d2541c433826ae
Necrom <diogofpombeiro@gmail.com>
2024-11-18 23:51:42 +0100

    Add About image links

--- app/controllers/about_controller.rb
+++ app/controllers/about_controller.rb
@@ -1,5 +1,8 @@
 class AboutController < ApplicationController
   def index
 		@band_members = @@data.dig(*%i[media about band_members])
+		@logo = @@data.dig(*%i[media about logo])
+		@background = @@data.dig(*%i[media about background])
+		@band = @@data.dig(*%i[media about band])
   end
 end
