f748ca032b3556c83817f8ecedfb35fda7e2167b
Necrom <diogofpombeiro@gmail.com>
2024-11-21 15:48:38 +0100

    Add date sorting to posters

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,8 @@
 class HomeController < ApplicationController
 	def index
-		@upcoming_shows = @@data.dig(*%i[media posters upcoming_shows])
-		@past_shows = @@data.dig(*%i[media posters past_shows])
+		@upcoming_shows = @@data.dig(*%i[media posters]).select { |show| Date.parse(show[:date]) >= Date.today }.sort_by { |show| Date.parse(show[:date]) }
+		@past_shows = @@data.dig(*%i[media posters]).select { |show| Date.parse(show[:date]) < Date.today }.sort_by { |show| Date.parse(show[:date]) }.reverse
+
 		@videos = @@data.dig(*%i[media videos])
 		@pictures = @@data.dig(*%i[media pictures])
 		@logo = @@data.dig(*%i[media home logo])
