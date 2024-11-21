34a601527443e59e4fe8fb2a1a3eeb50b20c060e
Necrom <diogofpombeiro@gmail.com>
2024-11-22 00:58:22 +0100

    Clean up Posters parser

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,10 @@
 class HomeController < ApplicationController
 	def index
-		@upcoming_shows = @@data.dig(*%i[media posters]).select { |show| Date.parse(show[:date]) >= Date.today }.sort_by { |show| Date.parse(show[:date]) }
-		@past_shows = @@data.dig(*%i[media posters]).select { |show| Date.parse(show[:date]) < Date.today }.sort_by { |show| Date.parse(show[:date]) }.reverse
+    @upcoming_shows, @past_shows = @@data.dig(*%i[media posters])
+      .map { |show| { **show, date: Date.parse(show[:date]) } }
+      .sort_by { |show| show[:date] }
+      .partition { |show| show[:date].future? }
+    @past_shows.reverse!
 
 		@videos = @@data.dig(*%i[media videos])
 		@pictures = @@data.dig(*%i[media pictures])
