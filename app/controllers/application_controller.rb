ae31ef60db85b82faac55bcfe2a3dadd0e441253
Diogo Pombeiro <diogofpombeiro@gmail.com>
2025-07-07 11:09:57 +0200

    fix: remove old browser restriction to allow Lighthouse testing and some bots

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,6 @@
 class ApplicationController < ActionController::Base
   include FilteredVisit
 
-  # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
-  allow_browser versions: :modern
-
   before_action :set_locale
   before_action :set_data
   before_action :set_visit_count
