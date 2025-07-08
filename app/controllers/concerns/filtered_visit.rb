2217539b108c45407a43fe4cb71a424739fc88e9
Diogo Pombeiro <diogofpombeiro@gmail.com>
2025-07-08 10:44:54 +0200

    feat(filtered_visits): exclude amazon hostnames

--- app/controllers/concerns/filtered_visit.rb
+++ app/controllers/concerns/filtered_visit.rb
@@ -11,8 +11,9 @@ module FilteredVisit
 
   def bot_visit?(visit)
     user_agent = visit.user_agent.to_s.downcase
+    platform = visit.platform.to_s.downcase
     browser = Browser.new(user_agent)
-    browser.bot? || user_agent.include?("http")
+    browser.bot? || user_agent.include?("http") || platform.include?("amazon")
   end
 
   IGNORED_COUNTRIES = %w[
