10604c12e70e9295bdf7db262f61e0fef7524bba
Diogo Pombeiro <diogofpombeiro@gmail.com>
2025-07-30 10:17:36 +0200

    fix(schema): use latest and only migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_07_01_094042) do
+ActiveRecord::Schema[8.0].define(version: 2025_06_17_073029) do
   create_table "ahoy_events", force: :cascade do |t|
     t.integer "visit_id"
     t.integer "user_id"
