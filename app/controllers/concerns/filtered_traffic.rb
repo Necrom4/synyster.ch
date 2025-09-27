315acbce0982576bad2403b34fbd4b4c1c1768b7
Diogo Pombeiro <diogofpombeiro@gmail.com>
2025-09-27 17:32:14 +0200

    perf(filtered_traffic): use SQL queries instead of ruby chaining for filter_visits

--- app/controllers/concerns/filtered_traffic.rb
+++ app/controllers/concerns/filtered_traffic.rb
@@ -4,39 +4,59 @@ module FilteredTraffic
   private
 
   def filter_visits
-    sql_filtered = Ahoy::Visit
+    Ahoy::Visit
       .where.not(ip: FILTERED_IPS)
       .where.not(country: FILTERED_COUNTRIES)
-      .where.not(
-        Ahoy::Visit.arel_table[:landing_page].matches_any(
-          FILTERED_URLS.map { |path| "%#{path}%" }
-        )
-      )
-      .where(
-        Ahoy::Visit.arel_table[:platform].matches_any(
-          FILTERED_HOSTNAME_KEYWORDS.map { |keyword| "%#{keyword.downcase}%" }
-        ).not.or(Ahoy::Visit.arel_table[:platform].eq(nil))
-      )
-      .where.not(
-        FILTERED_ORGANIZATION_KEYWORDS.map { |keyword| "LOWER(utm_campaign) LIKE ?" }.join(" OR "),
-        *FILTERED_ORGANIZATION_KEYWORDS.map { |keyword| "%#{keyword.downcase}%" }
-      )
-      .where.not(
-        FILTERED_USER_AGENT_KEYWORDS.map { |keyword| "LOWER(user_agent) LIKE ?" }.join(" OR "),
-        *FILTERED_USER_AGENT_KEYWORDS.map { |keyword| "%#{keyword.downcase}%" }
-      )
-
-    sql_filtered.reject { |visit| Browser.new(visit.user_agent).bot? }
+      .where(<<~SQL
+        landing_pages: FILTERED_URLS,
+        platform_keywords: FILTERED_HOSTNAME_KEYWORDS,
+        org_keywords: FILTERED_ORGANIZATION_KEYWORDS,
+        ua_keywords: FILTERED_USER_AGENT_KEYWORDS)
+        NOT EXISTS (
+            SELECT
+                1
+            FROM
+                unnest(:landing_pages) AS lp
+            WHERE
+                ahoy_visits.landing_page ILIKE '%' || lp || '%')
+            AND (ahoy_visits.platform IS NULL
+                OR NOT EXISTS (
+                    SELECT
+                        1
+                    FROM
+                        unnest(:platform_keywords) AS pk
+                    WHERE
+                        LOWER(ahoy_visits.platform)
+                        LIKE '%' || LOWER(pk) || '%'))
+                AND NOT EXISTS (
+                    SELECT
+                        1
+                    FROM
+                        unnest(:org_keywords) AS ok
+                    WHERE
+                        LOWER(ahoy_visits.utm_campaign)
+                        LIKE '%' || LOWER(ok) || '%')
+                    AND NOT EXISTS (
+                        SELECT
+                            1
+                        FROM
+                            unnest(:ua_keywords) AS ua
+                        WHERE
+                            LOWER(ahoy_visits.user_agent)
+                            LIKE '%' || LOWER(ua) || '%')
+      SQL
+            )
+      .reject { |visit| Browser.new(visit.user_agent).bot? }
   end
 
   def filter_events
     visit_filtered_events = Ahoy::Event.where(visit_id: filter_visits.map(&:id))
     multiple_visit_events = Ahoy::Event.where(
-      visit_id: Ahoy::Event .group(:visit_id) .having("COUNT(*) > 1") .pluck(:visit_id)
+      visit_id: Ahoy::Event.group(:visit_id)
+                           .having("COUNT(*) > 1")
+                           .pluck(:visit_id)
     )
-
-    (visit_filtered_events + multiple_visit_events)
-      .uniq { |event| event.id }
+    (visit_filtered_events + multiple_visit_events).uniq(&:id)
   end
 
   FILTERED_URLS = %w[
