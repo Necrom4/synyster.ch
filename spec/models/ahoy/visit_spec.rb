e81db5ee1e5b22a9da597027a4c76f971101ba5b
Diogo Pombeiro <diogofpombeiro@gmail.com>
2025-09-30 00:57:12 +0200

    test(analytics.filtered_visit): fill invalid visit test with all types of invalidities

--- spec/models/ahoy/visit_spec.rb
+++ spec/models/ahoy/visit_spec.rb
@@ -8,8 +8,20 @@ RSpec.describe Ahoy::Visit, type: :model do
     end
 
     it "excludes invalid visit" do
-      invalid_visit = create(:ahoy_visit, :filtered_country)
-      expect(Ahoy::Visit.filtered).not_to include(invalid_visit)
+      bot_visit = create(:ahoy_visit, :bot_user_agent)
+      invalid_ip_visit = create(:ahoy_visit, :filtered_ip)
+      invalid_country_visit = create(:ahoy_visit, :filtered_country)
+      invalid_landing_page_visit = create(:ahoy_visit, :filtered_landing_page)
+      invalid_hostname_visit = create(:ahoy_visit, :filtered_hostname)
+      invalid_user_agent_visit = create(:ahoy_visit, :filtered_user_agent)
+      invalid_org_name_visit = create(:ahoy_visit, :filtered_organization_name)
+      expect(Ahoy::Visit.filtered).not_to include(bot_visit)
+      expect(Ahoy::Visit.filtered).not_to include(invalid_ip_visit)
+      expect(Ahoy::Visit.filtered).not_to include(invalid_country_visit)
+      expect(Ahoy::Visit.filtered).not_to include(invalid_landing_page_visit)
+      expect(Ahoy::Visit.filtered).not_to include(invalid_hostname_visit)
+      expect(Ahoy::Visit.filtered).not_to include(invalid_user_agent_visit)
+      expect(Ahoy::Visit.filtered).not_to include(invalid_org_name_visit)
     end
   end
 end
