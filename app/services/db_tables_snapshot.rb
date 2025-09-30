269e84f93e2ed191e4b070ca1e637ea810736673
Diogo Pombeiro <diogofpombeiro@gmail.com>
2025-09-30 15:06:16 +0200

    refactor(analytics): move 'db_check' to services as DBTablesSnapshot

---
+++ app/services/db_tables_snapshot.rb
@@ -0,0 +1,23 @@
+class DbTablesSnapshot
+  def self.call
+    all_tables = ActiveRecord::Base.descendants.each_with_object({}) do |model, hash|
+      next if model.abstract_class? || !model.table_exists?
+
+      begin
+        hash[model.name] = model.order(id: :desc)
+      rescue
+        next
+      end
+    end
+
+    filtered_visits = Ahoy::Visit.filtered.sort_by { |visit| -visit.id }
+    filtered_events = Ahoy::Event.filtered.sort_by { |event| -event.id }
+
+    {
+      "Filtered Visits" => filtered_visits,
+      "Filtered Events" => filtered_events
+    }.merge(all_tables)
+  rescue *DB_ERRORS => e
+    {"error" => "Skipped DbTablesSnapshot.call due to database error: #{e.class} - #{e.message}"}
+  end
+end
