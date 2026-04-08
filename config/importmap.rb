854c35eba8e0aa4e99bc7a37c59f79a303df059e
Diogo Pombeiro <diogofpombeiro@gmail.com>
2026-04-08 23:47:05 +0200

    chore: update gems yarn and importmap

--- config/importmap.rb
+++ config/importmap.rb
@@ -5,8 +5,8 @@ pin "@hotwired/turbo-rails", to: "turbo.min.js"
 pin "@hotwired/stimulus", to: "stimulus.min.js"
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
-pin "@fancyapps/ui", to: "@fancyapps--ui.js" # @6.1.7
-pin "ahoy.js" # @0.4.4
+pin "@fancyapps/ui", to: "@fancyapps--ui.js" # @6.1.13
+pin "ahoy.js" # @0.4.5
 pin "ahoy/tracking", to: "ahoy/tracking.js"
 pin "utils/notify", to: "utils/notify.js"
 pin "bootstrap" # @5.3.8
